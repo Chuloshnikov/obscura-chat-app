c343f78991b8ab67a7f84b9e74f8a7b8f8edab38
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2025-05-05 12:45:01 +0300

    done

--- src/components/ChatContainer.tsx
+++ src/components/ChatContainer.tsx
@@ -48,41 +48,41 @@ const ChatContainer = () => {
       <ChatHeader />
 
       <div className="flex-1 overflow-y-auto p-4 space-y-4">
-        {messages.map((message) => (
-          <div
-            key={message._id}
-            className={`chat ${message.senderId === authUser._id ? "chat-end" : "chat-start"}`}
-            ref={messageEndRef}
-          >
-            <div className=" chat-image avatar">
-              <div className="size-10 rounded-full border">
-                <img
-                  src={
-                    message.senderId === authUser._id
-                      ? authUser.profilePic || "/avatar.png"
-                      : selectedUser.profilePic || "/avatar.png"
-                  }
-                  alt="profile pic"
-                />
+        {messages.map((message) => {
+          const isOwnMessage = message.senderId === authUser._id;
+          const bubbleStyle = isOwnMessage
+            ? "text-zinc-900 bg-zinc-300"
+            : "text-zinc-500";
+
+          return (
+            <div
+              key={message._id}
+              className={`w-full ${isOwnMessage ? "text-right" : "text-left"}`}
+              ref={messageEndRef}
+            >
+              {(message.text || message.image) && (
+                <div
+                  className={`border inline-block p-4 rounded my-1 max-w-[50%] break-words ${bubbleStyle}`}
+                >
+                  <div className="flex flex-col gap-2">
+                    {message.image && (
+                      <img
+                        src={message.image}
+                        alt="Attachment"
+                        className="sm:max-w-[200px] rounded-md"
+                      />
+                    )}
+                    {message.text && <p>{message.text}</p>}
+                  </div>
+                </div>
+              )}
+
+              <div className="text-xs text-gray-600 mt-1">
+                {formatMessageTime(message.createdAt)}
               </div>
             </div>
-            <div className="chat-header mb-1">
-              <time className="text-xs opacity-50 ml-1">
-                {formatMessageTime(message.createdAt)}
-              </time>
-            </div>
-            <div className="chat-bubble flex flex-col">
-              {message.image && (
-                <img
-                  src={message.image}
-                  alt="Attachment"
-                  className="sm:max-w-[200px] rounded-md mb-2"
-                />
-              )}
-              {message.text && <p>{message.text}</p>}
-            </div>
-          </div>
-        ))}
+          );
+        })}
       </div>
 
       <MessageInput />
