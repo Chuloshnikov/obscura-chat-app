d3204aab2cb5438bf88be9c5107d768bd9ee1fcf
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2025-05-05 16:44:59 +0300

    done

--- src/components/FormField.tsx
+++ src/components/FormField.tsx
@@ -1,39 +1,50 @@
+import { Controller, Control, FieldValues, Path } from "react-hook-form";
+
 import {
-    FormControl,
-    FormItem,
-    FormLabel,
-    FormMessage,
-  } from "@/components/ui/form"
-  import { Input } from "@/components/ui/input";
-  import { Control, Controller, FieldValues, Path } from "react-hook-form";
-  
-  interface FormFieldProps<T extends FieldValues> {
-    control: Control<T>
-    name: Path<T>;
-    label: string;
-    placeholder?: string;
-    type?: 'text' | 'email' | 'password' | 'file';
-  
-  }
-  
-  const FormField = ({ control, name, label, placeholder, type="text" }: FormFieldProps<T>) => (
-    <Controller 
-    name={name} 
-    control={control} 
-    render={({field}) => (
-      <FormItem>
-      <FormLabel>{label}</FormLabel>
-      <FormControl>
-          <Input 
-          placeholder={placeholder} 
-          type={type}
-          {...field} 
-          />
-      </FormControl>
-      <FormMessage />
-      </FormItem>
-    )}
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+// Define the props interface with a generic type T
+interface FormFieldProps<T extends FieldValues> {
+  control: Control<T>;
+  name: Path<T>;
+  label: string;
+  placeholder?: string;
+  type?: "text" | "email" | "password" | "file";
+}
+
+// Explicitly declare the generic type T for the component
+function FormField<T extends FieldValues>({
+  control,
+  name,
+  label,
+  placeholder,
+  type = "text",
+}: FormFieldProps<T>) {
+  return (
+    <Controller
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel className="label">{label}</FormLabel>
+          <FormControl>
+            <Input
+              className="input"
+              type={type}
+              placeholder={placeholder}
+              {...field}
+            />
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      )}
     />
-  )
-  
-  export default FormField;
+  );
+}
+
+export default FormField;
