af006a3fdfc5be765330a09a1f9c4fc7f41a3964
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2025-05-10 13:13:07 +0300

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 
 import { useAuthStore } from "./store/useAuthStore";
-import { useEffect } from "react";
 
 import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
@@ -8,19 +7,17 @@ import SignUpPage from "./pages/SignUpPage";
 import ProfilePage from "./pages/ProfilePage";
 import LoginPage from "./pages/LoginPage";
 
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import { Toaster } from "sonner";
 import { Loader } from "lucide-react";
+import { ProtectedRoute } from "./components/ProtectedRoute";
 
 
 function App() {
-  const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
-  useEffect(() => {
-    checkAuth();
-  }, [checkAuth]);
+  const { isCheckingAuth } = useAuthStore();
 
-  if (isCheckingAuth && !authUser) {
+  if (isCheckingAuth) {
     return (
       <div className="flex items-center justify-center h-screen">
         <Loader className="size-10 animate-spin"/>
@@ -28,24 +25,26 @@ function App() {
     )
   }
 
-  if (!authUser) {
-    return (
-     <Navigate to="/sign-in"/>
-    )
-  }
-
   return (
     <div>
-    <Navbar/>
+      <Navbar/>
       <Routes>
-          <Route path="/" element={authUser ? <HomePage/> : <Navigate to="/sign-in"/>}/>
-          <Route path="/sign-up" element={!authUser ? <SignUpPage/> : <Navigate to="/"/>}/>
-          <Route path="/sign-in" element={!authUser ? <LoginPage/> : <Navigate to="/"/>}/>
-          <Route path="/profile" element={authUser ? <ProfilePage/> : <Navigate to="/sign-in"/>}/>
+        <Route path="/" element={
+          <ProtectedRoute>
+            <HomePage />
+          </ProtectedRoute>
+        }/>
+        <Route path="/profile" element={
+          <ProtectedRoute>
+            <ProfilePage />
+          </ProtectedRoute>
+        }/>
+        <Route path="/sign-up" element={<SignUpPage />} />
+        <Route path="/sign-in" element={<LoginPage />} />
       </Routes>
-    <Toaster/>
-</div>
-  )
+      <Toaster />
+    </div>
+  );
 }
 
 export default App
---
+++ src/components/ProtectedRoute.tsx
@@ -0,0 +1,23 @@
+// components/ProtectedRoute.tsx
+import { Navigate } from "react-router-dom";
+import { useAuthStore } from "../store/useAuthStore";
+import { Loader } from "lucide-react";
+import { JSX } from "react";
+
+export const ProtectedRoute = ({ children }: { children: JSX.Element }) => {
+  const { authUser, isCheckingAuth } = useAuthStore();
+
+  if (isCheckingAuth) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Loader className="size-10 animate-spin" />
+      </div>
+    );
+  }
+
+  if (!authUser) {
+    return <Navigate to="/sign-in" replace />;
+  }
+
+  return children;
+};
